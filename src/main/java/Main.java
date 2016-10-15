67bb1da7c8391ed4645357aa8ea147cafd44f291
Dane Sisinni <dfsisinni@gmail.com>
2016-10-15 17:22:35 -0400

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        for (int i  883; i <= 968; i++) {
+    public static void main(String[] args) throws IOException {
+        for (int i = 883; i <= 968; i++) {
             String url = "http://www.westerncalendar.uwo.ca/2016/pg" + String.valueOf(i) + ".html";
             Document doc = Jsoup.connect(url).get();
         }
